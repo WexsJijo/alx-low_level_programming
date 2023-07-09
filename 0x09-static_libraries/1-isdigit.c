735e839f6cf5401655507632041314eced36ee91
WexsJijo <ngukhunasi@gmail.com>
2023-07-09 13:30:45 +0300

    Static Lib

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *_isdigit -  checks if c is a digit.
+ *
+ *@c: The character to be checked if digit.
+ *Return: (1) if c is a character, (0) otherwise.
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+	return (1);
+	}
+	return (0);
+}
